c9eb9192b5ddbd6e74ad7ff5233a26044aedef3e
urlockGaur <126732165+urlockGaur@users.noreply.github.com>
2024-10-30 14:06:47 -0500

    initial commit

---
+++ LegallyBuyAlcoholApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LegallyBuyAlcoholApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LegallyBuyAlcoholApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LegallyBuyAlcoholApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
